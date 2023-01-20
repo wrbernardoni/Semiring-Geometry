402a6ee3222e21ae45230e3122b89851cbb36ec6
Billy B <wrbernardoni@gmail.com>
2023-01-20 16:11:48 -0500

    Toy example for hop limiting

--- tropicalDstar.cpp
+++ tropicalDstar.cpp
@@ -109,5 +109,77 @@ int main()
 	starNoStorage = Semiring::Matrix<Semiring::ContactSemiring<Semiring::BooleanSemiring, 16>,4,4>::One() + noStorage + noStorage * noStorage + noStorage * noStorage * noStorage;
 	cout << starNoStorage(0,3) << endl;
 
+	cout << endl;
+
+	Semiring::HopLimitedSemiring<3> n(1);
+	cout << n << endl;
+	cout << n * n << endl;
+	cout << n * n * n << endl;
+	cout << n * n * n * n << endl;
+	cout << endl;
+
+
+
+	Semiring::Matrix<Semiring::ContactSemiring<Semiring::HopLimitedSemiring<5>, 10>,5,5> FourSteps;
+	for (int i = 0; i < 4; i++)
+	{
+		FourSteps(i,i) = Semiring::ContactSemiring<Semiring::HopLimitedSemiring<5>, 10>([](unsigned int t, unsigned int d)->Semiring::HopLimitedSemiring<5>{
+			if (d <= 1)
+				return Semiring::HopLimitedSemiring<5>(1);
+			else
+				return Semiring::HopLimitedSemiring<5>::Zero();
+		});
+	}
+
+	FourSteps(0,1) = Semiring::ContactSemiring<Semiring::HopLimitedSemiring<5>, 10>([](unsigned int t, unsigned int d)->Semiring::HopLimitedSemiring<5>{
+		if ((t <= 2) && (t >= 0) && (d == 2))
+			return Semiring::HopLimitedSemiring<5>(1);
+		else
+			return Semiring::HopLimitedSemiring<5>::Zero();
+	});
+
+	FourSteps(1,2) = Semiring::ContactSemiring<Semiring::HopLimitedSemiring<5>, 10>([](unsigned int t, unsigned int d)->Semiring::HopLimitedSemiring<5>{
+		if ((t <= 3) && (t >= 3) && (d == 2))
+			return Semiring::HopLimitedSemiring<5>(1);
+		else
+			return Semiring::HopLimitedSemiring<5>::Zero();
+	});
+
+	FourSteps(2,3) = Semiring::ContactSemiring<Semiring::HopLimitedSemiring<5>, 10>([](unsigned int t, unsigned int d)->Semiring::HopLimitedSemiring<5>{
+		if ((t <= 10) && (t >= 6) && (d == 2))
+			return Semiring::HopLimitedSemiring<5>(1);
+		else
+			return Semiring::HopLimitedSemiring<5>::Zero();
+	});
+
+	FourSteps(0,4) = Semiring::ContactSemiring<Semiring::HopLimitedSemiring<5>, 10>([](unsigned int t, unsigned int d)->Semiring::HopLimitedSemiring<5>{
+		if ((d == 3))
+			return Semiring::HopLimitedSemiring<5>(1);
+		else
+			return Semiring::HopLimitedSemiring<5>::Zero();
+	});
+
+	FourSteps(4,3) = Semiring::ContactSemiring<Semiring::HopLimitedSemiring<5>, 10>([](unsigned int t, unsigned int d)->Semiring::HopLimitedSemiring<5>{
+		if ((t >= 8) && (d == 1))
+			return Semiring::HopLimitedSemiring<5>(1);
+		else
+			return Semiring::HopLimitedSemiring<5>::Zero();
+	});
+
+	Semiring::Matrix<Semiring::ContactSemiring<Semiring::HopLimitedSemiring<5>, 10>,5,5> pK = Semiring::Matrix<Semiring::ContactSemiring<Semiring::HopLimitedSemiring<5>, 10>,5,5>::One() + FourSteps;
+	for (int i = 0; i < 10; i++)
+	{
+		pK = pK * pK;
+	}
+
+	cout << "1->2" << endl;
+	cout << pK(0,1)  << endl;
+	cout << "1->3" << endl;
+	cout << pK(0,2)  << endl;
+	cout << "1->4" << endl;
+	cout << pK(0,3)  << endl;
+	cout << "1->5" << endl;
+	cout << pK(0,4)  << endl;
+
 	return 0;
 }
