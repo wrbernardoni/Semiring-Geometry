26771b7cbb8df179398a98801ccaf6d7b5b8a86f
Billy B <wrbernardoni@gmail.com>
2023-02-21 21:23:17 -0500

    Added verbose setting to pathfinding and sped up reduntant base check

--- Utilities/ShortestPath.h
+++ Utilities/ShortestPath.h
@@ -7,10 +7,20 @@
 
 #include <vector>
 
+#define VERBOSE 1
+
 namespace Semiring
 {
-	std::vector<std::vector<int>> ChoiceBuilder(int N, int C, int minE = 0)
+	std::vector<std::vector<int>> ChoiceBuilder(int N, int C, int minE, Semiring::FreeIdempotentSemiring<Semiring::FreeMonoid<0>> built, std::vector<Semiring::FreeIdempotentSemiring<Semiring::FreeMonoid<0>>>& constraints)
 	{
+		for (int l = 0; l < constraints.size(); l++)
+		{
+			if (built <= constraints[l])
+			{
+				return std::vector<std::vector<int>>();
+			}
+		}
+
 		if (C > 1)
 		{
 			// if (C > (N - minE))
@@ -20,7 +30,7 @@ namespace Semiring
 
 			for (int  i = minE; i < N; i++)
 			{
-				auto subC = ChoiceBuilder(N, C - 1, i+1);
+				auto subC = ChoiceBuilder(N, C - 1, i+1, built + Semiring::FreeIdempotentSemiring<Semiring::FreeMonoid<0>>(Semiring::FreeMonoid<0>(i + 1)), constraints);
 				for (int j = 0; j < subC.size(); j++)
 				{
 					std::vector<int> c;
@@ -38,9 +48,21 @@ namespace Semiring
 			std::vector<std::vector<int>> choices;
 			for (int i = minE; i < N; i++)
 			{
-				std::vector<int> c;
-				c.push_back(i);
-				choices.push_back(c);
+				bool redundant = false;
+				for (int l = 0; l < constraints.size(); l++)
+				{
+					if ((built + Semiring::FreeIdempotentSemiring<Semiring::FreeMonoid<0>>(Semiring::FreeMonoid<0>(i + 1))) <= constraints[l])
+					{
+						redundant = true;
+						break;
+					}
+				}
+				if (!redundant)
+				{
+					std::vector<int> c;
+					c.push_back(i);
+					choices.push_back(c);
+				}
 			}
 			return choices;
 		}
@@ -72,6 +94,9 @@ namespace Semiring
 	template<typename CostType, unsigned int N>
 	Semiring::Matrix<Semiring::FreeIdempotentSemiring<Semiring::FreeIdempotentSemiring<Semiring::FreeMonoid<N * N>>>,N,N> MinimalPaths(Semiring::Matrix<CostType,N,N> costMatrix, unsigned int max_steps)
 	{
+		#ifdef VERBOSE
+			std::cout << "\tSetting up path matrix" << std::endl;
+		#endif
 		Semiring::Matrix<Semiring::FreeIdempotentSemiring<Semiring::FreeIdempotentSemiring<Semiring::FreeMonoid<N * N>>>,N,N> pathMatrix([&costMatrix](unsigned int t, unsigned int d)->Semiring::FreeIdempotentSemiring<Semiring::FreeIdempotentSemiring<Semiring::FreeMonoid<N * N>>> {
 			if (costMatrix(t,d) != CostType::Zero())
 				return Semiring::FreeIdempotentSemiring<Semiring::FreeIdempotentSemiring<Semiring::FreeMonoid<N * N>>>(Semiring::FreeIdempotentSemiring<Semiring::FreeMonoid<N * N>>(Semiring::FreeMonoid<N * N>(t * N + d + 1)));
@@ -94,19 +119,30 @@ namespace Semiring
 			return s;
 		};
 
-
+		#ifdef VERBOSE
+			std::cout << "\tSetting up step matrices" << std::endl;
+		#endif
 		Semiring::Matrix<CostType,N,N> costStep = costMatrix + Semiring::Matrix<CostType,N,N>::One();
 		pathMatrix =  pathMatrix + Semiring::Matrix<Semiring::FreeIdempotentSemiring<Semiring::FreeIdempotentSemiring<Semiring::FreeMonoid<N * N>>>,N,N>::One();
 		auto pathStep = pathMatrix;
 		auto cMatrix = costStep;
+		#ifdef VERBOSE
+			std::cout << "\tMatrices created" << std::endl;
+		#endif
 
 		for (int i = 1; i <= max_steps; i++)
 		{
 			// Reduce paths
+			#ifdef VERBOSE
+				std::cout << "\tReducing path possibilities to minimal bundles" << std::endl;
+			#endif
 			for (int t = 0; t < N; t++)
 			{
 				for (int d = 0; d < N; d++)
 				{
+					#ifdef VERBOSE
+						std::cout << "\t\t Reducing entry ("<< t << "," << d << ")" << std::endl;
+					#endif
 					std::unordered_set<Semiring::FreeIdempotentSemiring<Semiring::FreeMonoid<N * N>>, StreamHash<Semiring::FreeIdempotentSemiring<Semiring::FreeMonoid<N * N>>>> availPaths;
 
 					auto opSet = pathMatrix(t,d).getSet();
@@ -122,29 +158,39 @@ namespace Semiring
 
 					for (int C = 1; C <= bases.size(); C++)
 					{
+						#ifdef VERBOSE
+							std::cout << "\t\t\tComputing "<< C << " element choices of " << bases.size() << " bases\r" << std::flush;
+						#endif
 						// Number of choices
-						auto choices = ChoiceBuilder(bases.size(), C);
+						auto choices = ChoiceBuilder(bases.size(), C,0, Semiring::FreeIdempotentSemiring<Semiring::FreeMonoid<0>>(), usedBases);
+						#ifdef VERBOSE
+							std::cout << "\t\t\tComputing "<< C << " element choices (" << choices.size() << ") of " << bases.size() << " bases\r" << std::flush;
+						#endif
+
 						for (int c = 0; c < choices.size(); c++)
 						{
+							#ifdef VERBOSE
+								std::cout << "\t\t\tComputing "<< C << " element choices (" << c + 1 << "/" << choices.size() << ") of " << bases.size() << " bases\r" << std::flush;
+							#endif
 							Semiring::FreeIdempotentSemiring<Semiring::FreeMonoid<0>> bU;
 							for (int k = 0; k < choices[c].size(); k++)
 							{
 								bU.insert(choices[c][k] + 1);
 							}
-							if (C > 1)
-							{
-								bool redundant = false;
-								for (int l = 0; l < usedBases.size(); l++)
-								{
-									if (bU <= usedBases[l])
-									{
-										redundant = true;
-										break;
-									}
-								}
-								if (redundant)
-									continue;
-							}
+							// if (C > 1)
+							// {
+							// 	bool redundant = false;
+							// 	for (int l = 0; l < usedBases.size(); l++)
+							// 	{
+							// 		if (bU <= usedBases[l])
+							// 		{
+							// 			redundant = true;
+							// 			break;
+							// 		}
+							// 	}
+							// 	if (redundant)
+							// 		continue;
+							// }
 
 							// Check for redundancy
 							std::unordered_set<Semiring::FreeMonoid<N * N>, StreamHash<Semiring::FreeMonoid<N * N>>> choice;
@@ -179,24 +225,40 @@ namespace Semiring
 							}
 						}
 					}
+					#ifdef VERBOSE
+						if (bases.size() > 0)
+							std::cout << std::endl;
+					#endif
 
 					pathMatrix(t,d) = Semiring::FreeIdempotentSemiring<Semiring::FreeIdempotentSemiring<Semiring::FreeMonoid<N * N>>>(availPaths);
 				}
 			}
 
-			std::cout << "Step " << i << std::endl;
-			std::cout << pathMatrix << std::endl;
+			#ifdef VERBOSE
+				std::cout << "\tStep " << i << std::endl;
+				std::cout << pathMatrix << std::endl;
+			#endif
+			
 			
 
 			// Take a step
+			#ifdef VERBOSE
+				std::cout << "\tCost matrix multiplication step" << std::endl;
+			#endif
 			auto nC = cMatrix * costStep;
 			if (nC == cMatrix)
 			{
 				return pathMatrix;
 			}
 
+			#ifdef VERBOSE
+				std::cout << "\tPath matrix multiplication step" << std::endl;
+			#endif
 			auto nP = pathMatrix * pathStep;
 
+			#ifdef VERBOSE
+				std::cout << "\tReducing to prior entries in path matrix for unchanged cost" << std::endl;
+			#endif
 			for (int t = 0; t <N; t++)
 			{
 				for (int d = 0; d < N; d++)
