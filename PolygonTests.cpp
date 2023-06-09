44afd238eb0bccc965bbf04e5cd2404a15926546
Billy B <wrbernardoni@gmail.com>
2023-06-09 10:53:38 -0400

    Added some "tests" with the empty collections and polygons

--- PolygonTests.cpp
+++ PolygonTests.cpp
@@ -15,9 +15,13 @@ int main()
 	cout << "Hello world!" << endl;
 
 	Polygon emptyPoly;
-	cout << "Empty polygon: " << emptyPoly << endl;
+	cout << "Empty polygon (should be []): " << emptyPoly << endl;
 
 	PolygonCollection emptyCollection;
-	cout << "Empty polygon collection: " << emptyCollection << endl;
+	cout << "Empty polygon collection (should be {}): " << emptyCollection << endl;
+
+	cout << "Adding empty polygon to empty collection (should still be empty): " << emptyCollection.Add(emptyPoly) << endl;
+	cout << "Is the empty polygon a subset of the empty collection (should be 1): " << SubsetEq(emptyPoly, emptyCollection) << endl;
+	cout << "Is the empty collection a subset of the empty polygon (should be 1): " << SubsetEq(emptyCollection, emptyPoly) << endl;
 	return 0;
 }
