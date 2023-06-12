d5644509e7b9e479223b42f13f1fab94fcf6251f
Billy B <wrbernardoni@gmail.com>
2023-06-12 09:08:23 -0400

    Added some more test lines for when polygon code is written

--- PolygonTests.cpp
+++ PolygonTests.cpp
@@ -24,7 +24,13 @@ int main()
 	cout << "Is the empty polygon a subset of the empty collection (should be 1): " << SubsetEq(emptyPoly, emptyCollection) << endl;
 	cout << "Is the empty collection a subset of the empty polygon (should be 1): " << SubsetEq(emptyCollection, emptyPoly) << endl;
 
-	cout << "Unit box: " << Polygon::UnitBox() << endl;
-	cout << "Vertical halfplane offset by 1/2: " << Polygon::HalfPlane(Point(0,0.5), Point(1,0.5));
+	Polygon uBox = Polygon::UnitBox();
+	Polygon hPlane = Polygon::HalfPlane(Point(0,0.5), Point(1,0.5));
+	cout << "Unit box: " << uBox << endl;
+	cout << "Vertical halfplane offset by 1/2: " << hPlane << endl;
+	cout << "Union of the above: " << Union(uBox, hPlane) << endl;
+	cout << "Intersection of the above: " << Intersect(uBox, hPlane) << endl;
+	cout << "Box minus halfplane: " << SetDifference(uBox, hPlane) << endl;
+	cout << "Halfplane minus box: " << SetDifference(hPlane, uBox) << endl;
 	return 0;
 }
